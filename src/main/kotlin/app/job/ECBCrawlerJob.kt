57c0dd7227111e4999834086b24081a573190106
Thibault Meyer <meyer.thibault@gmail.com>
2020-07-29 19:01:21 +0200

    Catch errors on ECB crawler
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/main/kotlin/app/job/ECBCrawlerJob.kt
+++ src/main/kotlin/app/job/ECBCrawlerJob.kt
@@ -9,11 +9,14 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.w3c.dom.Document
 import java.io.BufferedInputStream
+import java.io.FileNotFoundException
 import java.io.InputStream
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.net.ConnectException
 import java.net.URL
 import java.net.URLConnection
+import java.net.UnknownHostException
 import javax.inject.Singleton
 import javax.xml.parsers.DocumentBuilderFactory
 
@@ -31,24 +34,32 @@ class ECBCrawlerJob(private val currencyService: CurrencyService) {
     @Scheduled(cron = "0 2 * * *")
     fun refreshCurrencyRates() {
         logger.info("Refresh currency exchange rate from European Central Bank")
-        val url = URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
-        val urlConnection: URLConnection = url.openConnection()
-        val inputStream: InputStream = BufferedInputStream(urlConnection.getInputStream())
-        val dbFactory = DocumentBuilderFactory.newInstance()
-        val dBuilder = dbFactory.newDocumentBuilder()
-        val doc: Document = dBuilder.parse(inputStream)
+        try {
+            val url = URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
+            val urlConnection: URLConnection = url.openConnection()
+            val inputStream: InputStream = BufferedInputStream(urlConnection.getInputStream())
+            val dbFactory = DocumentBuilderFactory.newInstance()
+            val dBuilder = dbFactory.newDocumentBuilder()
+            val doc: Document = dBuilder.parse(inputStream)
 
-        val nodes = doc.getElementsByTagName("Cube")
-        for (idx in 0 until nodes.length) {
-            val node = nodes.item(idx)
+            val nodes = doc.getElementsByTagName("Cube")
+            for (idx in 0 until nodes.length) {
+                val node = nodes.item(idx)
 
-            if (node.attributes.getNamedItem("currency") != null) {
-                val currency = node.attributes.getNamedItem("currency").nodeValue
-                val rate = BigDecimal(node.attributes.getNamedItem("rate").nodeValue)
+                if (node.attributes.getNamedItem("currency") != null) {
+                    val currency = node.attributes.getNamedItem("currency").nodeValue
+                    val rate = BigDecimal(node.attributes.getNamedItem("rate").nodeValue)
 
-                val currencyRate = CurrencyRate(rateToBase = EUR_BASE_CURRENCY_RATE.divide(rate, 10, RoundingMode.HALF_DOWN), rateFromBase = rate)
-                currencyService.updateCurrencyRate(currency = Currency.valueOf(currency), currencyRate = currencyRate)
+                    val currencyRate = CurrencyRate(rateToBase = EUR_BASE_CURRENCY_RATE.divide(rate, 10, RoundingMode.HALF_DOWN), rateFromBase = rate)
+                    currencyService.updateCurrencyRate(currency = Currency.valueOf(currency), currencyRate = currencyRate)
+                }
             }
+        } catch (ex: ConnectException) {
+            logger.error("Cannot refresh currency exchange rate from European Central Bank: {}", ex.message)
+        } catch (_: FileNotFoundException) {
+            logger.error("Cannot refresh currency exchange rate from European Central Bank: 404 File Not Found")
+        } catch (ex: UnknownHostException) {
+            logger.error("Cannot refresh currency exchange rate from European Central Bank: Unknown Host {}", ex.message)
         }
     }
 }
